541a65549e6ac84db876a11c5c58cee590052693
Nursultan Askarbekuly <nursultan@outlook.com>
2022-06-28 23:50:57 +0300

    simplify the assets protocol

--- PersonalizationKit/ViewAssets.swift
+++ PersonalizationKit/ViewAssets.swift
@@ -13,19 +13,11 @@ protocol PersonalizationAssets {
     var mainTextColor: UIColor { get }
     var descriptionTextColor: UIColor { get }
     var detailsTextColor: UIColor { get }
-    var copyrightTextColor: UIColor { get }
-    var placeholderTextColor: UIColor { get }
     var primaryColor: UIColor { get }
     var primaryLightColor: UIColor { get }
     var primaryLighterColor: UIColor { get }
     var buttonTextColor: UIColor { get }
     var borderColor: UIColor { get }
-    var completeProgressColor: UIColor { get }
-    var pinkAccentColor: UIColor { get }
-    var successMain: Color { get }
-    var successLighter: Color { get }
-    var errorMain: Color { get }
-    var errorLighter: Color { get }
     var checkboxFull: String { get }
     var checkboxEmpty: String { get }
 }
